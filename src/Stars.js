e06cf5dd9248e451992073112ce5817d727270db
KrystianKaminski <krystiankaminski07@gmail.com>
2019-01-22 21:05:50 +0100

    Added stars from FontAwesome library

--- src/Stars.js
+++ src/Stars.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faStar)
 
 const Stars = (props) => (
     <div>
-        ...
+        <FontAwesomeIcon icon="star" />
+        <FontAwesomeIcon icon="star" />
+        <FontAwesomeIcon icon="star" />
+        <FontAwesomeIcon icon="star" />
     </div>
 )
 
